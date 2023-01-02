518b2d5c1b26de21b5201825f6933631019336a1
nayarunda <ezranyagaka96@gmail.com>
2023-01-02 21:36:20 +0300

    Define tables and database connection

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connection { get; private set; }
+        public static void InnializerConnection(bool database, bool textFiles)
+        {
+            if(database)
+            {
+                //TODO create SQL Conncetion
+
+            } else if(textFiles)
+            {
+                //TODO Create text connection
+            }
+        }
+
+    }
+}
